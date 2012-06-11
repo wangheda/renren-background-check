1875768714995741b9c761a93fc1a824f2c3a488
Wangheda <whd.thu@gmail.com>
2012-06-11 10:26:52 +0800

    input ok

--- renren-background-check/main.ui.js
+++ renren-background-check/main.ui.js
@@ -1,1 +1,12 @@
 // JavaScript Document
+
+$(document).ready(function(){
+	var window_width = window.innerWidth;
+	var window_height = window.innerHeight;
+	var panel_width = parseInt(window_width/5)
+	$('div.display').css('height', window_height-2*parseInt($('div.display').css('padding-top')));
+	$('div.display').css('margin-right', panel_width);
+	$('div.friend_panel').css('height', window_height);
+	$('div.friend_panel').css('width', panel_width);
+	
+});
